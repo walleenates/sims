57355e9647976630ef3c79174718a89384a4bf26
Walleen.Ates <walleenates.202100119@gmail.com>
2024-09-05 19:38:48 +0800

    walleen

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,23 +1,17 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
-import './Sidebar.css'; // Ensure this path is correct based on your file structure
 
-function Sidebar() {
+import './Sidebar.css'
+
+function Sidebar({ SidebarLinks }) {
   return (
     <div className="sidebar">
-      <div className="logo-section">
-        <img src="/path/to/logo.png" alt="Logo" className="logo" />
-        <img src="/path/to/small-logo.png" alt="Small Logo" className="small-logo" />
-      </div>
-      <div className="links-section">
-        <Link to="/dashboard">Dashboard</Link>
-        <Link to="/manage-item">Manage Item</Link>
-        <Link to="/request-form">Request Form</Link>
-        <Link to="/purchased-request">Purchased Request</Link>
-        <Link to="/settings">Settings</Link>
-        <Link to="/reports">Reports</Link>
-        <Link to="/scanner">Scanner</Link>
-      </div>
+      Sidebar
+      {SidebarLinks.map((link) => (
+          <Link key={link.tag} to={link.href}>
+            {link.tag}
+          </Link>
+        ))}
     </div>
   );
 }
